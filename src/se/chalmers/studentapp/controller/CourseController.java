e631a1912a689099482cbf81f5081119726f831f
Matz Larsson <matzlarsson94@gmail.com>
2015-01-23 11:22:28 +0100

    Added right click functionality

--- src/se/chalmers/studentapp/controller/CourseController.java
+++ src/se/chalmers/studentapp/controller/CourseController.java
@@ -1,22 +1,122 @@
 package se.chalmers.studentapp.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import se.chalmers.studentapp.model.Course;
 
 public class CourseController {
+	
+	private static CourseController inst = new CourseController();
+	private static StateStamp[] stamps = {inst.new StateStamp("completed", "#5fd359"),
+										  inst.new StateStamp("mandatory", "#d61b0a"),
+										  inst.new StateStamp("queued", "#ed5e00"),
+										  inst.new StateStamp("registered", "#ffdb00", Color.BLACK)};
 
+	@FXML
+	private Pane courseRoot;
 	@FXML
 	private Label code;
 	@FXML
 	private Label name;
 	@FXML
 	private Label credits;
+	@FXML
+	private Label stateLabel;
 	
-	public void setCourse(Course course){
+	public void setCourse(Course course, State state){
 		code.setText(course.getCode());
 		name.setText(course.getName());
 		credits.setText(course.getCredits()+" HP");
+		stateLabel.setText(stamps[state.getNum()].getLabel());
+		stateLabel.setStyle(stateLabel.getStyle()+"-fx-background-color:"+stamps[state.getNum()].getColor());
+		stateLabel.textFillProperty().set(stamps[state.getNum()].getTextColor());
+		
+		final ContextMenu contextMenu = getContextMenu(state);
+	    courseRoot.setOnMouseClicked(new EventHandler<MouseEvent>() {
+	        @Override
+	        public void handle(MouseEvent mouseEvent) {
+	            contextMenu.show(courseRoot, mouseEvent.getScreenX(), mouseEvent.getScreenY());
+	        }
+	    });
+	}
+
+	private void register(){
+		System.out.println("Registering "+code.getText());
 	}
 	
+	private void unregister(){
+		System.out.println("Unregistering "+code.getText());
+	}
+	
+	private void unqueue(){
+		System.out.println("Leaving queue for "+code.getText());
+	}
+	
+	private ContextMenu getContextMenu(State state){
+		ContextMenu menu = new ContextMenu();
+		switch(state){
+			case COMPLETED:		break;
+			case REGISTERED:	MenuItem unregister = new MenuItem("Unregister");
+								unregister.setOnAction((ActionEvent event) -> {unregister();});
+								menu.getItems().add(unregister);
+								break;
+			case MANDATORY:		MenuItem register = new MenuItem("Register");
+								register.setOnAction((ActionEvent event) -> {register();});
+								menu.getItems().add(register);
+								break;
+			case QUEUED:		MenuItem unqueue = new MenuItem("Leave queue");
+								unqueue.setOnAction((ActionEvent event) -> {unqueue();});
+								menu.getItems().add(unqueue);
+								break;
+			default:			System.out.println("WARNING: UNIMPLEMENTED STATE!");
+		}
+		
+		return menu;
+	}
+	
+	private class StateStamp{
+		private String label;
+		private String color;
+		private Color textColor;
+		
+		public StateStamp(String label, String color){
+			this(label, color, Color.WHITE);
+		}
+		public StateStamp(String label, String color, Color textColor){
+			this.label = label;
+			this.color = color;
+			this.textColor = textColor;
+		}
+		public String getLabel(){
+			return label;
+		}
+		public String getColor(){
+			return color;
+		}
+		public Color getTextColor(){
+			return textColor;
+		}
+	}
+	
+	public enum State{
+		COMPLETED(0),
+		MANDATORY(1),
+		QUEUED(2),
+		REGISTERED(3);
+		
+		private int num;
+		private State(int num){
+			this.num = num;
+		}
+		public int getNum(){
+			return num;
+		}
+	}
 }
--- src/se/chalmers/studentapp/controller/FrontController.java
+++ src/se/chalmers/studentapp/controller/FrontController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import se.chalmers.studentapp.controller.CourseController.State;
 import se.chalmers.studentapp.model.Course;
 import se.chalmers.studentapp.util.ViewUtil;
 
@@ -22,19 +23,19 @@ public class FrontController {
 	private VBox requiredCoursePanel;
 	
 	public void initialize(){
-		addCourse(new Course("TDA357", "Databases and stuff", 7.5), "completed", true);
-		addCourse(new Course("MVE051", "Matematisk statistik", 7.5), "registered", true);
-		addCourse(new Course("BLA098", "Kurs om blablabla", 7.5), "queued", false);
-		addCourse(new Course("KON433", "Kontroversiell kurs", 7.5), "mandatory", false);
-		addCourse(new Course("TRI322", "Tripplekvadratisk mojs", 7.5), "mandatory", false);
+		addCourse(new Course("TDA357", "Databases and stuff", 7.5), State.COMPLETED, true);
+		addCourse(new Course("MVE051", "Matematisk statistik", 7.5), State.REGISTERED, true);
+		addCourse(new Course("BLA098", "Kurs om blablabla", 7.5), State.QUEUED, false);
+		addCourse(new Course("KON433", "Kontroversiell kurs", 7.5), State.MANDATORY, false);
+		addCourse(new Course("TRI322", "Tripplekvadratisk mojs", 7.5), State.MANDATORY, false);
 	}
 	
-	private void addCourse(Course course, String type, boolean active){
+	private void addCourse(Course course, State state, boolean active){
 		VBox box = (active?activeCoursePanel:requiredCoursePanel);
-		box.getChildren().add(ViewUtil.getNodeFromFXML(type+"course", new ViewUtil.Function(){
+		box.getChildren().add(ViewUtil.getNodeFromFXML("course", new ViewUtil.Function(){
 			public <T> void perform(T controller){
 				if(controller instanceof CourseController){
-					((CourseController)controller).setCourse(course);
+					((CourseController)controller).setCourse(course, state);
 				}
 			}
 		}));
